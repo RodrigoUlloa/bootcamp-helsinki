330233d984aa086d315103dea2d1f6875d6b2f71
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2023-12-14 18:18:35 -0300

    ejercicios 1.1 - 12 OK

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,18 +1,36 @@
-import './App.css'
-import Mensaje from './Mensaje'
 
 
-const Description = () => {
-  return <p>Esta es la app del curso fullstack bootcamp</p>
+const Title = ({course}) => {
+//   // const {course} = props == const course = props.course
+   return <h1>{course}</h1>
 }
 
+const Part = ({part, exercise}) => {
+  return <p>{part} {exercise}</p>
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+  return <p> Number of exercise {exercises1 + exercises2 + exercises3} </p>
+}
+
+// const Title = ({course}) => <h1>{course}</h1>
+
 function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a compnent'
+  const exercises3 = 14
+
   return (
     <>
-      <Mensaje color='red' message='Tamos trajando'/>      
-      <Mensaje color='green' message='En un curso'/>      
-      <Mensaje color='blue' message='de React'/>
-      <Description/>
+      <Title course={course} />
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </>
   )
 }
